1af625e72ac1530511c731736290a40e83b26469
Rzhek <vladkurgankin2004@gmail.com>
2023-04-11 16:36:17 -0400

    Sending Email done, gitignore

---
+++ script.js
@@ -0,0 +1,29 @@
+import {publicKey, serviceId, templateId} from "./config.js";
+
+(function(){
+    emailjs.init(publicKey);
+
+    document.getElementById("contact-btn").onclick = sendMail;
+
+})();
+
+export function sendMail(){
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+        reply_to: document.getElementById("replyTo").value
+    };
+        
+    emailjs.send(serviceId, templateId, params)
+    .then(
+        res => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("Your message was sucessfully sent! Thank you!");
+        }
+    )
+}
+    
